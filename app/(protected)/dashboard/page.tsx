7ef09b3a9428be5e16452495b70e2efdaa522183
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-04-24 16:30:10 -0400

    it was adapted dashboard for technicians roles

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,17 +1,15 @@
+import Dashboard from "@/app/components/Dashboard";
+import DashboardTech from "@/app/components/DashboardTech";
 import HeaderSideBar from "@/app/components/HeaderSideBar";
+import { auth } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <>
       <HeaderSideBar before="Inicio" title="Dashboard" href="/dashboard" />
-      <div className="flex flex-1 flex-col gap-4 p-4">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-        </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-      </div>
+      {session && session.user.role === "ADMIN" && <Dashboard />}
+      {session && session.user.role === "TECHNICIAN" && <DashboardTech />}
     </>
   );
 }
