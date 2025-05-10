fae4806d72c6a9b0501ce0bb8db300e7dd4e7c17
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-05-10 10:43:48 -0400

    return the before configuration

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,1 @@
-export const API_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "http://127.0.0.1:3000"; // Usa localhost dentro del mismo contenedor
+export const API_URL = "https://apiglobal.ajedev.com"
