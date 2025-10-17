de36d8b32387a867c182c7f06166b28b5ea8871d
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-10-17 12:42:18 -0400

    add  session control

--- app/(protected)/projects/[id]/edit/page.tsx
+++ app/(protected)/projects/[id]/edit/page.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { fetchAllTechsInWork, fetchOneProject, fetStepsToWorkByIdWork } from "@/app/lib/orders-data";
+import {
+  fetchAllTechsInWork,
+  fetchOneProject,
+  fetStepsToWorkByIdWork,
+} from "@/app/lib/orders-data";
 import FormEditProject from "@/app/components/projects/FormEditProject";
 import HeaderSideBar from "@/app/components/HeaderSideBar";
 import { fetchAllCompanies } from "@/app/lib/company-data";
@@ -13,8 +17,14 @@ import DiagDeleteStepToWork from "@/app/components/projects/DiagDeleteStepToWork
 import DiagAddTechToWork from "@/app/components/projects/DiagAddTechToWork";
 import { fetchAllTechs } from "@/app/lib/user-data";
 import DiagDeleteTechFromWork from "@/app/components/projects/DiagDeleteTechFromWork";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 const EditProjectPage = async (props: { params: Promise<{ id: string }> }) => {
+  const session = await auth();
+  if (session?.user.role !== "ADMIN") {
+    return redirect("/");
+  }
   const params = await props.params;
   const project = await fetchOneProject(params.id);
   const client = await fetchOneClient(project.company.clientsId);
@@ -22,7 +32,7 @@ const EditProjectPage = async (props: { params: Promise<{ id: string }> }) => {
   const steps: Steps[] = await fetStepsToWorkByIdWork(params.id);
   const techniciansInWork: Technicians[] = await fetchAllTechsInWork(params.id);
   const technicians: User[] = await fetchAllTechs();
-  console.log("technicians", technicians);
+
   return (
     <>
       <HeaderSideBar
@@ -56,19 +66,23 @@ const EditProjectPage = async (props: { params: Promise<{ id: string }> }) => {
                     idWork={params.id}
                     technicians={technicians}
                   />
-                  {techniciansInWork.length > 0 && techniciansInWork.map((tech) => (
-                    <div
-                      key={tech.technician.id}
-                      className="flex  justify-between border w-full"
-                    >
-                      <div className="flex items-center  border px-2 py-1 w-[70%] md:w-[90%] ">
-                        {tech.technician.name}
+                  {techniciansInWork.length > 0 &&
+                    techniciansInWork.map((tech) => (
+                      <div
+                        key={tech.technician.id}
+                        className="flex  justify-between border w-full"
+                      >
+                        <div className="flex items-center  border px-2 py-1 w-[70%] md:w-[90%] ">
+                          {tech.technician.name}
+                        </div>
+                        <div className="flex flex-col md:flex-row items-center border p-2 w-[30%] md:w-[10%] md:gap-4 gap-4 justify-center">
+                          <DiagDeleteTechFromWork
+                            techId={tech.technician.id}
+                            workId={project.id}
+                          />
+                        </div>
                       </div>
-                      <div className="flex flex-col md:flex-row items-center border p-2 w-[30%] md:w-[10%] md:gap-4 gap-4 justify-center">
-                        <DiagDeleteTechFromWork techId={tech.technician.id} workId={project.id} />
-                      </div>
-                    </div>
-                  ))}
+                    ))}
                 </fieldset>
               </div>
               <div className="w-[100%] p-0 md:p-2 mt-4">
