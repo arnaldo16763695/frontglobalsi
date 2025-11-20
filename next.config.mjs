a8c4ccb834fb9aa54d2bf50f73b130b587ee3ad9
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-11-20 16:39:04 -0400

    setup Imageurl

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',       
+      },
+      {
+        protocol: 'https',
+        hostname: 'umq27iem9h.ufs.sh',       
+      },
+    ],
+  },
 };
 
 export default nextConfig;
