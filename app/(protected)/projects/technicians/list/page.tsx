53b97c270dc11b5d2a79bb3c05e2fa4d17e9951a
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-11-23 18:29:32 -0400

    refactoring

--- app/(protected)/projects/technicians/list/page.tsx
+++ app/(protected)/projects/technicians/list/page.tsx
@@ -17,7 +17,7 @@ const TechProjectsList = async () => {
       />
 
       <div className="flex flex-1 flex-col gap-4 p-4">
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+        <div className="w-full max-w-[100vw] overflow-x-auto">
           <DataTableOrdersTech columns={columns} data={data} /> 
         </div>
       </div>
